72ba9e0bc7eeb6cdc8b8778c9cdb525622139fb4
sdfsdf <sdfsdfsdfsdfsdfsdf>
2025-10-21 09:47:28 +0200

    we

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // console.log('hello world!');
+
+//boolean
+true;
+false;
+
+//number
+3;
+3.14;
+-34.4;
+Infinity;
+-Infinity;
+
+NaN; // Not a number
+
+//String 
+'ciaoooooo'
+"fratm"
+"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita mollitia ullam quasi delectus dolor a sed consequatur dolorum cumque perferendis. Incidunt cumque facere amet debitis recusandae deleniti soluta ducimus! Perferendis!"
+'a'
+'' // stringhe possono essere vuote
+
+// tipi nulli
+undefined // variabile mai stata definita
+null // nullo (variabile vuota)

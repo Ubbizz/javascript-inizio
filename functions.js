1b901a8cc53f3c4828c56a7070db9087ff06ab8c
sdfsdf <sdfsdfsdfsdfsdfsdf>
2025-10-28 10:46:20 +0100

    iuctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctctct

--- functions.js
+++ functions.js
@@ -13,12 +13,12 @@
 //                          // dopo il return il parametro in output
 
 //     const result = n * 3;
-    
+
 //     return result;
 // }
 
 // const multiplyBy4 = (n) => { // definizione lambda (arrow function) della funzione equivalente alle precedenti
-    
+
 //     const result = n * 4;
 
 //     return result;
@@ -34,7 +34,7 @@
 // }
 
 // const checkMultipleLambda = (n1, n2) => {
-    
+
 //     const isMultiple = n1 % n2 === 0;
 
 //     return isMultiple;
@@ -89,3 +89,119 @@
 // console.log(paperinik);
 
 // logUppercase('viva le funzioni!!!')
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// function stringBuilder(s1, s2, s3) {
+
+//     return s1 + s2 + s3;
+
+// }
+
+// console.log(stringBuilder("leonardo", "leo", "nardo"));
+
+// console.log(stringBuilder("leonardo", "leo")); // parametri in meno risulta undefined
+
+// console.log(stringBuilder("leonardo", "leo", "nardo", "onar")); //parametri in più non li conta
+
+
+// function pow(base = 4, exponent = 2) {
+
+//     // if (exponent == undefined) {
+//     //     exponent = 2
+//     // }
+
+//     return base ** exponent;
+
+// }
+
+// console.log(pow(3, 3));
+// console.log(pow(3)); //NaN se non specifichi base o esponente, o il "0" o "2" nella funzione come comportamento standardù
+// console.log(pow());
+
+
+////////////////////////////////////// BINDINGS AND SSCOPE //////////////////////////////////
+
+
+// const number = 5
+
+// if( number > 3) {
+
+//     const number2 = number + 5;
+
+//     console.log(number2);
+
+//     if (number2 > 7) {
+
+//         const number3 = number2 + 5
+
+//         console.log(number3);
+
+//     }
+
+//     // console.log(number3); //scope esterno non può accedere alle variabili di quello interno, in questo caso number3
+//     // non solo non li vede, ma "non esistono", se fra 100 righe voglio usare "number3" non me lo trova
+// }
+
+// const number3 = number2 +5 
+
+// console.log(number3); //da number2 not defined perchè il codice dentro le parentesi graffe (figlio) può vedere il codice del padre (fuori parentesi) ma non viceversa
+// // lo scope esterno non accede alle variabili dello scope interno
+
+
+////// CLOJURE ////// eccezione
+
+
+// const isOpen = true;
+
+// let myFunction; // scope "padre"
+
+// if (isOpen) {
+
+//     let counter = 0 // scope figlio
+
+//     myFunction = () => {
+//         counter++;
+//         console.log(counter);
+
+//     } // posso richiamare "counter" perchè l'ho definito prima delle graffe
+
+//     // myFunction()
+//     // myFunction()
+//     // myFunction()
+//     // myFunction()
+
+// }
+
+// myFunction() 
+// myFunction()
+// myFunction()
+// myFunction() //funziona comunque perchè le funzioni se utilizzano una variabile esterna al suo scope, rimangono comunque in "vita"
+
+
+////// RECURSION ////// possibilità di una funzione di richiamare se stessa es: fattoriale (!) n! = n * (n -1)! // 0! = 1 ricorda
+// in informatica si dice fact
+// fact(n) = n * fact(n-1);
+
+
+function fact(n) {
+
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
+    }
+
+}
+
+console.log(fact(0));
+console.log(fact(1));
+console.log(fact(5));
+
+
+
+
+
+
